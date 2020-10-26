6b023b1770836590916536c16a91d5af3cdfe7a6
Sun <syseoz@naver.com>
2020-10-26 17:15:08 +0900

    socket.io 기본

--- nodejs_12/socket.js
+++ nodejs_12/socket.js
@@ -1,27 +1,24 @@
-const WebSocket = require('ws');
+const SocketIO = require('socket.io');
 
 module.exports = (server)=>{
-    const wss = new WebSocket.Server({server});
+    const io = SocketIO(server,{path:'/socket.io'});
 
-    wss.on('connection',(ws,req)=>{
+    io.on('connection',(socket)=>{
+        const req = socket.request;
         const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        console.log('새로운 클라이언트 접속',ip);
-        ws.on('message',(message)=>{
-            console.log(message);
+        console.log('새로운 클라이언트 접속',ip,socket.id,req.ip);
+        socket.on('disconnection',()=>{
+            console.log('클라이언트 접속 해제',ip,socket.id);
+            clearInterval(socket.interval);
         });
-        ws.on('error',(error)=>{
+        socket.on('error',(error)=>{
             console.log(error);
         })
-        ws.on('close',()=>{
-            console.log('클라이언트 접속 해제',ip);
-            clearInterval(ws.interval);
-        });
-
-
-        ws.interval = setInterval(()=>{//3초마다 클라이언트로 메시지 전송
-            if(ws.readyState === ws.OPEN){
-                ws.send('서버에서 클라이언트로 메시지를 보냅니다');
-            }
+        socket.on('reply',(data)=>{
+            console.log(data);
+        })
+        socket.interval=setInterval(()=>{
+            socket.emit('news','Hello');
         },3000);
-    });
+    })
 };
